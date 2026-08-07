64034e681e1d9debd5e2ad975f345ac0157d8446
shamahinu-cell <shamahinu@gmail.com>
2026-08-07 22:04:48 +0530

    First commit

---
+++ Project1/src/com/tnsif/basics/DataTypesDemo.java
@@ -0,0 +1,18 @@
+package com.tnsif.basics;
+
+public class DataTypesDemo {
+	public static void main(String[] args) {
+		int value1 = 9/2;
+		float value2 = 101/61;
+		double value3 = 10d/6d;
+		float value4 = 5/2.5f;
+		System.out.println("value1=" +value1);
+		System.out.println("value2=" +value2);
+		System.out.println("value3=" +value3);
+		System.out.println("value4=" +value4);
+		int marker = 512;
+		double percentage = marker*0.46f;
+		System.out.println("percentage=" +percentage);
+	}
+
+}
