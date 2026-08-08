abe5ad3494a5f0aa8da2d0b3716f3e2e7d30b715
shamahinu-cell <shamahinu@gmail.com>
2026-08-08 21:46:20 +0530

    First commit

---
+++ Project1/src/com/java/basics/IdentifiersDemo.java
@@ -0,0 +1,12 @@
+package com.java.basics;
+
+public class IdentifiersDemo {
+	public static void main(String[] args) {
+		int $number = 20;
+		System.out.println("Value of the number variable is: "+$number);
+		String studentName = "Uzma";
+		System.out.println("Value is: "+studentName);
+		
+	}
+
+}
