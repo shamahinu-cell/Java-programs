770898f670852f7d57250549ed044a4d9d267a89
shamahinu-cell <shamahinu@gmail.com>
2026-08-09 18:53:32 +0530

    First commit

---
+++ Project1/src/com/java/basics/ContinueDemo.java
@@ -0,0 +1,12 @@
+package com.java.basics;
+
+public class ContinueDemo {
+	public static void main(String[] args) {
+		for(int k = 5; k < 15; k++) {
+			if(k%2 != 0)
+				
+				System.out.println(k+"");
+		}
+	}
+
+}
