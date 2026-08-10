7a11f886d94e0cf4c3a4c3acdf70d54c96283fb0
shamahinu-cell <shamahinu@gmail.com>
2026-08-10 18:26:51 +0530

    First commit

---
+++ Project1/src/com/java/basics/DoWhileDemo.java
@@ -0,0 +1,12 @@
+package com.java.basics;
+
+public class DoWhileDemo {
+	public static void main(String[] args) {
+		int i = 10;
+		do {
+			System.out.println(i);
+			i++;
+		}while(i<=15);
+	}
+
+}
