d060b2735e1609f810d895edb94d46992bc849b8
shamahinu-cell <shamahinu@gmail.com>
2026-08-10 18:35:28 +0530

    First commit

---
+++ Project1/src/com/java/basics/IfElseDemo.java
@@ -0,0 +1,12 @@
+package com.java.basics;
+
+public class IfElseDemo {
+	public static void main(String[] args) {
+		int age = 11;
+		if(age>18)
+			System.out.println("You are eligible for voting ");
+		else
+			System.out.println("Sorry! You are not eligible for voting");
+	}
+
+}
